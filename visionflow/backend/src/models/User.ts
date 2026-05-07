d19305da98c589c55fc834fdb1b87b9b3beb8354
andreahuncikova <andhun01@easv365.dk>
2026-05-07 13:56:21 +0200

    add user model

---
+++ visionflow/backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+export interface IUser extends Document {
+  _id: mongoose.Types.ObjectId
+  username: string
+  email: string
+  password: string
+  createdAt: Date
+  comparePassword(candidatePassword: string): Promise<boolean>
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minlength: 6 },
+  },
+  { timestamps: true }
+)
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  this.password = await bcrypt.hash(this.password, 12)
+  next()
+})
+
+UserSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password)
+}
+
+export default mongoose.model<IUser>('User', UserSchema)
